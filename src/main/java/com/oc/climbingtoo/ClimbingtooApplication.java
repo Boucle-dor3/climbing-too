a162b403cceb271214e08220f49a2041c87d1746
Boucle-dor3 <campari.alexandra@gmail.com>
2018-05-30 16:51:56 +0200

    disable thymeleaf cache

--- src/main/java/com/oc/climbingtoo/ClimbingtooApplication.java
+++ src/main/java/com/oc/climbingtoo/ClimbingtooApplication.java
@@ -1,7 +1,18 @@
 package com.oc.climbingtoo;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.io.IOException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class ClimbingtooApplication {
@@ -9,7 +20,26 @@ public class ClimbingtooApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClimbingtooApplication.class, args);
 	}
+
+	@Autowired
+	private ThymeleafProperties properties;
+
+	@Value("${spring.thymeleaf.templates_root:}")
+	private String templatesRoot;
+
+
+	@Bean
+	public ITemplateResolver defaultTemplateResolver() {
+		FileTemplateResolver resolver = new FileTemplateResolver();
+		resolver.setSuffix(properties.getSuffix());
+		resolver.setPrefix(templatesRoot);
+		resolver.setTemplateMode(properties.getMode());
+		resolver.setCacheable(properties.isCache());
+		return resolver;
+	}
 }
+
+
 
 
 
